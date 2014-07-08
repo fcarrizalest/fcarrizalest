f52a3d15b61909099d719bab9a9140bbf53700ec
fcarrizalest <fcarrizalest@gmail.com>
2014-07-08 03:13:00 -0500

    flatt

--- node.tpl.php
+++ node.tpl.php
@@ -44,7 +44,7 @@
 													<div class="fb-share-button" data-href="<?php print $node_url; ?>" data-height="100"  data-width="120"  data-type="box_count"></div>
 													<div class="g-plus" data-action="share" data-annotation="vertical-bubble" data-height="60" data-href="http://fcarrizalest.com/<?php print $node_url; ?>"></div>
 													<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://fcarrizalest.com/<?php print $node_url; ?>" data-text="<?php print $title; ?>" data-via="Fcarrizalest" data-lang="es">Twittear</a>
-
+<script id='fb38cos'>(function(i){var f,s=document.getElementById(i);f=document.createElement('iframe');f.src='//api.flattr.com/button/view/?uid=fcarrizalest&url='+encodeURIComponent(document.URL);f.title='Flattr';f.height=62;f.width=55;f.style.borderWidth=0;s.parentNode.insertBefore(f,s);})('fb38cos');</script>
 													</div>
 
 												 <?php hide($content['field_image']);
